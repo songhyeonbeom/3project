fb7930bd0b4a13b42f9e81f6cc02dd8e971b3a8e
songhyeonbeom <erekude@naver.com>
2022-04-26 18:10:33 +0900

    25

--- newpro/config/asgi.py
+++ newpro/config/asgi.py
@@ -12,22 +12,6 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import os
 
 from django.core.asgi import get_asgi_application
--- onetoonefield/common/models.py
+++ onetoonefield/common/models.py
@@ -7,15 +7,13 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-
-
 class Profile(models.Model):
     user = models.OneToOneField(auth.models.User, on_delete=models.CASCADE, related_name='profile')
-    realname = models.TextField(max_length=15, null=True)       # 이름
-    birth_date = models.DateField(null=True)                    # 생년월일
-    gender = models.TextField(max_length=5, null=True)          # 성별
-    address = models.TextField(max_length=30, null=True)        # 주소
-    phone = models.TextField(max_length=15, null=True)          # 핸드폰
+    realname = models.TextField(max_length=15, null=True)  # 이름
+    birth_date = models.DateField(null=True)  # 생년월일
+    gender = models.TextField(max_length=5, null=True)  # 성별
+    address = models.TextField(max_length=30, null=True)  # 주소
+    phone = models.TextField(max_length=15, null=True)  # 핸드폰
 
 
 @receiver(post_save, sender=auth.models.User)
@@ -27,5 +25,3 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=auth.models.User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
-
-
