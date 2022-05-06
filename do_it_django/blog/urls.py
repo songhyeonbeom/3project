14f3ecb7cfa92f24059a199fd6559bf9ee22c307
songhyeonbeom <erekude@naver.com>
2022-05-06 16:56:30 +0900

    57

--- do_it_django/blog/urls.py
+++ do_it_django/blog/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
     # path('', views.index),
-    path('<int:pk>/', views.single_post_page),
+    # path('<int:pk>/', views.single_post_page),
 ]
 
 
--- do_it_django/blog/views.py
+++ do_it_django/blog/views.py
@@ -1,8 +1,8 @@
 import imp
-from django.shortcuts import render
-from .models import Post
+# from django.shortcuts import render
 # Create your views here.
-from django.views.generic import ListView
+from .models import Post
+from django.views.generic import ListView, DetailView
 
 
 
@@ -13,12 +13,8 @@ class PostList(ListView):
     # template_name = 'blog/index.html'
 
 
-
-
-
-
-
-
+class PostDetail(DetailView):
+    model = Post
 
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
@@ -28,10 +24,10 @@ class PostList(ListView):
 #     )
 
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
     
-    return render(request, 'blog/single_post_page.html', {'post': post,} )
+#     return render(request, 'blog/single_post_page.html', {'post': post,} )
 
 
 
