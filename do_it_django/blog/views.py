c0a49d48883945844f49838c7a6ff93af004a96b
songhyeonbeom <erekude@naver.com>
2022-05-06 16:48:20 +0900

    56

--- do_it_django/blog/views.py
+++ do_it_django/blog/views.py
@@ -2,14 +2,30 @@ import imp
 from django.shortcuts import render
 from .models import Post
 # Create your views here.
+from django.views.generic import ListView
 
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
         
-    return render(
-        request, 'blog/index.html', {'posts': posts,}
-    )
+    # template_name = 'blog/index.html'
+
+
+
+
+
+
+
+
+
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+        
+#     return render(
+#         request, 'blog/index.html', {'posts': posts,}
+#     )
 
 
 def single_post_page(request, pk):
