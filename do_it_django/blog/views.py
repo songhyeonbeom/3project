78d4667c27164d4eec514a79167e7d8bd4bce837
songhyeonbeom <erekude@naver.com>
2022-05-06 14:46:03 +0900

    53

--- do_it_django/blog/views.py
+++ do_it_django/blog/views.py
@@ -1,3 +1,19 @@
+import imp
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+
+def index(request):
+    posts = Post.objects.all().order_by('-pk')
+        
+    return render(
+        request, 'blog/index.html',
+    {
+        'posts': posts,
+    }
+    )
+
+
+
+
