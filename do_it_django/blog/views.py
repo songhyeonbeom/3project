ba8d2268471cadcee5240fc26c158e45d323c47d
songhyeonbeom <erekude@naver.com>
2022-05-16 15:19:43 +0900

    79

--- do_it_django/blog/views.py
+++ do_it_django/blog/views.py
@@ -1,8 +1,9 @@
-import imp
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post, Category
+
 # from django.shortcuts import render
 # Create your views here.
-from .models import Post, Category
-from django.views.generic import ListView, DetailView
 
 
 
@@ -15,6 +16,10 @@ class PostList(ListView):
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category= None).count()
         return context
+
+
+
+
     
 
 class PostDetail(DetailView):
@@ -28,20 +33,25 @@ class PostDetail(DetailView):
 
 
 
-# def index(request):
-#     posts = Post.objects.all().order_by('-pk')
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
         
-#     return render(
-#         request, 'blog/index.html', {'posts': posts,}
-#     )
-
-
-# def single_post_page(request, pk):
-#     post = Post.objects.get(pk=pk)
-    
-#     return render(request, 'blog/single_post_page.html', {'post': post,} )
-
-
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list' : post_list,
+            'categories' : Category.objects.all(),
+            'no_category_post_count' : Post.objects.filter(category=None).count(),
+            'category' : category,
+        }
+    )
 
 
 
