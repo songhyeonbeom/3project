47d4fc95b4cd0b68fbf34890fc0c4e546b1fef81
songhyeonbeom <erekude@naver.com>
2022-05-18 15:48:17 +0900

    86

--- do_it_django/blog/models.py
+++ do_it_django/blog/models.py
@@ -7,7 +7,7 @@ import os
 
 class Tag(models.Model):
     name = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+    slug = models.SlugField(max_length=200, unique=False, allow_unicode=True)
     
     def __str__(self):
         return self.name
