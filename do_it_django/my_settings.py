a57de6550abea967c9da90f1074021c9430c030b
songhyeonbeom <erekude@naver.com>
2022-05-25 12:22:25 +0900

    배포판 개발판 구분하지 몬했습니다..ㅠㅠ

--- do_it_django/my_settings.py
+++ do_it_django/my_settings.py
@@ -1,10 +1,30 @@
+import os
+import pymysql  
+pymysql.install_as_MySQLdb()
+import my_settings
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'project_django',
-        'USER': 'root',
-        'PASSWORD': 'rootroot',
-        'HOST': '192.168.10.34',
-        'PORT': '3306',
+        'ENGINE': os.environ.get("SQL_ENGINE", "django.db.backends.mysql"),
+        'NAME': os.environ.get('SQL_DATABASE'),
+        'USER': os.environ.get('SQL_USER'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD'),
+        'HOST': os.environ.get('SQL_HOST'),
+        'PORT': os.environ.get("SQL_PORT"),
         }
     }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'project_django',
+#         'USER': 'root',
+#         'PASSWORD': 'rootroot',
+#         'HOST': '192.168.10.34',
+#         'PORT': '3306',
+#         }
+#     }
