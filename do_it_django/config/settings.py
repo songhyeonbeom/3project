273fd6d81602d9cde0682cfaf37d43eafdbbadba
songhyeonbeom <erekude@naver.com>
2022-05-27 13:55:41 +0900

    템플릿 분리시키기

--- do_it_django/config/settings.py
+++ do_it_django/config/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, '_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
